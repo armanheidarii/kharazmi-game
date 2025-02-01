06dfea02e4b29faa402aac7c88ba987cf603f5d0
armanheids <armanheids@gmail.com>
2025-02-02 02:53:31 +0330

    CORS off

--- main/__init__.py
+++ main/__init__.py
@@ -1,12 +1,14 @@
 import os
 from dotenv import load_dotenv
 from flask import Flask
+from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 
 load_dotenv()
 
 app = Flask(__name__)
 app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
+CORS(app)
 JWTManager(app)
 
 import main.db
