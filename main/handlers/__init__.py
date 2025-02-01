75b091563f125669f7542d8b12eab3a593a02aaa
armanheids <armanheids@gmail.com>
2025-02-01 22:33:00 +0330

    handlers updated

--- main/handlers/__init__.py
+++ main/handlers/__init__.py
@@ -2,4 +2,5 @@ import main.handlers.signup
 import main.handlers.login
 import main.handlers.game
 import main.handlers.max_game_levels
-import main.handlers.max_game_categories
+
+# import main.handlers.max_game_categories
