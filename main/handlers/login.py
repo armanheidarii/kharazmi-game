10cc00c03af5fc70355fa2536173d22aeaac0c4e
armanheids <armanheids@gmail.com>
2025-02-01 22:22:01 +0330

    sign-in/sing-up updated

--- main/handlers/login.py
+++ main/handlers/login.py
@@ -1,8 +1,8 @@
+from peewee import DoesNotExist
 from flask import jsonify, request
 from flask_jwt_extended import create_access_token
-from peewee import DoesNotExist
-from main.db.models.User import User
 
+from main.db.models.User import User
 from main import app
 
 
@@ -11,13 +11,19 @@ def login():
     username = request.json.get("username")
     password = request.json.get("password")
 
+    if not username:
+        return jsonify({"error": 1, "msg": "Username cannot be empty!"}), 400
+
+    if not password:
+        return jsonify({"error": 2, "msg": "Password cannot be empty!"}), 400
+
     try:
         user = User.get(User.username == username)
         if user.password != password:
-            return jsonify({"error": 2, "msg": "Password is incorrect!"}), 401
+            return jsonify({"error": 4, "msg": "Password is incorrect!"}), 401
 
     except DoesNotExist:
-        return jsonify({"error": 1, "msg": "Username was not exist!"}), 401
+        return jsonify({"error": 3, "msg": "Username was not exist!"}), 401
 
     access_token = create_access_token(identity=user.username)
 
--- main/handlers/signup.py
+++ main/handlers/signup.py
@@ -1,7 +1,8 @@
+from peewee import DoesNotExist
 from flask import jsonify, request
+
 from main.db.models.User import User
 from main.db.models.KharazmiClass import KharazmiClass
-
 from main import app
 
 
@@ -12,20 +13,33 @@ def signup():
     email = request.json.get("email")
     class_name = request.json.get("class_name")
 
-    if User.select().where(User.username == username).exists():
-        return jsonify({"error": 1, "msg": "Username already exists!"}), 400
+    if not username:
+        return jsonify({"error": 1, "msg": "Username cannot be empty!"}), 400
+
+    if not password:
+        return jsonify({"error": 2, "msg": "Password cannot be empty!"}), 400
+
+    if not class_name:
+        return jsonify({"error": 3, "msg": "Class name cannot be empty!"}), 400
+
+    try:
+        User.get(User.username == username)
+        return jsonify({"error": 4, "msg": "Username already exists!"}), 400
+
+    except DoesNotExist:
+        pass
 
     if len(password) < 6:
-        return jsonify({"error": 2, "msg": "Password is incorrect!"}), 400
+        return jsonify({"error": 5, "msg": "Password is incorrect!"}), 400
 
     if email and not validators_email(email):
-        return jsonify({"error": 3, "msg": "Email is invalid!"}), 400
+        return jsonify({"error": 6, "msg": "Email is invalid!"}), 400
 
     class_ref = (
         KharazmiClass.select().where(KharazmiClass.class_name == class_name).exists()
     )
     if not class_ref:
-        return jsonify({"error": 4, "msg": "Class name was not exist!"}), 400
+        return jsonify({"error": 7, "msg": "Class name was not exist!"}), 400
 
     user = User.create(
         username=username,
